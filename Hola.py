3ac49803e4543d333c445d113566a1fa46e26106
Andres <jefreyalejandro28@gmail.com>
2026-02-10 10:12:51 -0500

    Lineas de dialogo y bienvenida

--- Hola.py
+++ Hola.py
@@ -1,2 +1,14 @@
 greeting = 'Hola Mundo, como estan!'
 print(greeting)
+
+linea01 = " ******************* "
+linea02 = " *                 * "
+linea03 = " *   BIENVENIDO    * "
+#Comentario de desarrolador 
+print(linea01)
+print(linea02)
+print(linea03)
+print(linea02)
+print(linea01)
+
+print (" Mi nombre es Andres y quiero aprender Python! ")
